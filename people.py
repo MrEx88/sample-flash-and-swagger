c54e288577e3c7fc04fc6636a892833b7931334d
jon.wesneski <jon.wesneski@salientsys.com>
2018-06-12 15:52:23 -0500

    fixed whitespace

--- people.py
+++ people.py
@@ -8,7 +8,8 @@ from flask import (
 
 def get_timestamp():
     return datetime.now().strftime(('%Y-%m-%d %H:%M:%S'))
-
+    
+    
 # Data to serve to api
 PEOPLE = {
     'Wesneski': {
@@ -33,31 +34,31 @@ PEOPLE = {
 def read():
     """This function responds to a request for /api/people
     with the complete lists of people
-
+    
     Returns:
         sorted list of people"""
     return [PEOPLE[key] for key in sorted(PEOPLE.keys())]
-
-
+    
+    
 def read_one(lname):
     """ Gets the person that matches the name.
-
+    
     Returns:
         person with same last name"""
     if lname in PEOPLE:
         return PEOPLE[lname]
     else: 
         abort(404, 'Person with last name {lname} not found'.format(lname=lname))
-
-
+        
+        
 # POST (people.create)
 def create(person):
     """Create a new person in the people structure basesd
     on the passed in person data
-
+    
     Args:
         person - person to create in people structure
-    
+        
     Returns:
         201 on success, 406 if person exists"""
     lname = person.get('lname', None)
@@ -71,8 +72,8 @@ def create(person):
         return make_response('{lname} successfully created'.format(lname=lname), 201)
     else:
         abort(406, 'Person with last name {lname} already exists'.format(lname=lname))
-
-
+        
+        
 # PUT (people.update)
 def update(lname, person):
     """Updates a person who matches the last name.
@@ -90,12 +91,12 @@ def update(lname, person):
         return PEOPLE[lname]
     else:
         abort(404, "Person with last name {lname} doesn't exist".format(lname=lname))
-
-
+        
+        
 # DELETE (people.delete)
 def delete(lname):
     """Deletes the person from the people list
-
+    
     Returns:
         201 on success, 404 if person doesn't exist."""
     try:
